36b49dfe02749ff9276eb61935ab53fdeeef4e60
Svitlana <fortestingdev2022@gmail.com>
2023-12-20 18:31:57 +0200

    1

--- main.py
+++ main.py
@@ -13,18 +13,20 @@ class Pers:
         self.image = image
         self.speed = speed
 
-    def move(self):
+    def move(self, key_left, key_right, key_up, key_down):
         k = pygame.key.get_pressed()
-        if k[pygame.K_d]:
+        if k[key_right]:
             if self.rect.right <= win_w:
                 self.rect.x += self.speed 
-        elif k[pygame.K_a]:
+        elif k[key_left]:
             if self.rect.left >= 0:
                 self.rect.x -= self.speed
-        elif k[pygame.K_w]:
-            self.rect.y -= self.speed
-        elif k[pygame.K_s]:
-            self.rect.y += self.speed
+        elif k[key_up]:
+            if self.rect.y >= 0:
+                self.rect.y -= self.speed
+        elif k[key_down]:
+            if self.rect.bottom <= win_h:
+                self.rect.y += self.speed
         
     
     def update(self):
@@ -41,15 +43,19 @@ background = pygame.transform.scale(background, (win_w, win_h))
 window.blit(background, (0, 0))
 
 pers_img = pygame.image.load("sprite1.png")
+pers1_img = pygame.image.load("sprite2.png")
 pers1 = Pers(250, 250, 50, 50, pers_img, 5)
+pers2 = Pers(150, 50, 50, 50, pers1_img, 5)
 
 game = True
 while game:
     window.blit(background, (0, 0))
     
-    pers1.move()
+    pers1.move(pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_s)
     pers1.update()
-    
+    pers2.move(pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN)
+    pers2.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
